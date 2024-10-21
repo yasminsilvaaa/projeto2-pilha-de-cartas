844466143b409f0e2a92d40c3d35d7c628341699
yasmin silva <yasmin.nascimento50@aluno.ce.gov.br>
2024-10-21 16:55:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-const waitingLine = {}
+const cards = []
+let option = '0';
+do (option !== '3') {
+    alert(`a quantidade de cartas no baralho: ${cards.length}\n1. Para adicionar uma carta.\n2. puxar uma carta\n3. sair`);
+    option = prompt('escolha uma opção entre 1-3:')
+    if (option === '1')
+        const cardName = prompt("Digite o nome da carta que deseja adicionar")
+    if (cardName) {
+
+        
+    }
+
+} 
